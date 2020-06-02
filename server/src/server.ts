fc0954f1fe0ad6b2722dbe04bbbbfdebebd6c4ad
Henrique Martins <hmartins224@gmail.com>
2020-06-02 13:26:32 -0300

    Adding routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app =  express();
 
-app.get('/test', (request, response) => {
-  response.json({
-    name: "henrique"
-  })
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
