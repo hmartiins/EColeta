40d88e93cbd9815d8a742aff6d5f0939655ab732
Henrique Martins <hmartins224@gmail.com>
2020-06-02 23:01:24 -0300

    List filtered collection points and fixing some returns

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -2,7 +2,24 @@ import knex from '../database/connection';
 import { Request, Response} from 'express';
 
 class PointsController {
-  async create (request: Request, response: Response){
+  async index(request: Request, response: Response){
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+      
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*')
+
+    return response.json(points);
+  }
+  async create(request: Request, response: Response){
     const { 
       name, 
       email, 
@@ -39,6 +56,8 @@ class PointsController {
     
       await trx('point_items').insert(pointItems);
     
+      await trx.commit();
+
       return response.json({
         id: insertedIds[0],
         ...point,
@@ -47,6 +66,22 @@ class PointsController {
       console.log(err);
       response.status(400).send({error: 'Operation failed'});
     }
+  }
+  async show(request: Request, response: Response){
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if(!point){
+      return response.status(400).send({message: 'point not found'});
+    }
+  
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({point, items});
   }
 }
 export default PointsController;
