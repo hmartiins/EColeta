b2985b142e9cb918c562168bc0cfbe61e0282b38
Henrique Martins <hmartins224@gmail.com>
2020-06-04 12:49:09 -0300

    Setting status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Home from './src/pages/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Salve bianca</Text>
-    </View>
+    <>
+      <StatusBar 
+        barStyle="dark-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
